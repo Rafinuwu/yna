3b0dedea41b39f4fed130eb4ce337c52a9b3c09d
Rafin Tanzim <66791918+Rafinuwu@users.noreply.github.com>
2024-06-03 21:20:58 +0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,9 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const cloudBackground = document.getElementById("cloud-background");
     const text1 = document.getElementById("text1");
     const text2 = document.getElementById("text2");
-    const heartContainer = document.getElementById("heart-container");
     const text3 = document.getElementById("text3");
-    const yeanaPhoto = document.getElementById("yeana-photo");
+    const heart = document.getElementById("heart");
 
     let step = 0;
 
@@ -23,9 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function loopAnimation() {
         if (step === 0) {
-            plane.style.display = "block";
-            cloudBackground.style.display = "block";
-            showText(text1, 5000);
+            showText(text1, 0);
             step++;
         } else if (step === 1) {
             fadeOut(text1, 2000);
@@ -33,20 +30,9 @@ document.addEventListener("DOMContentLoaded", () => {
             step++;
         } else if (step === 2) {
             fadeOut(text2, 2000);
-            showText(heartContainer, 4000);
-            showText(text3, 5000);
-            showText(yeanaPhoto, 6000);
+            showText(text3, 4000);
+            showText(heart, 4000);
             step++;
-        } else if (step === 3) {
-            plane.style.display = "none";
-            cloudBackground.style.display = "none";
-            text1.style.display = "none";
-            text2.style.display = "none";
-            heartContainer.style.display = "none";
-            text3.style.display = "none";
-            yeanaPhoto.style.display = "none";
-            step = 0;
-            loopAnimation();
         }
     }
 
